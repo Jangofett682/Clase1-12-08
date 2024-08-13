9d46cbcfa6b75c157c9e27d2420325a07ca68899
Lorenzo Paparo <lolopaparo@gmail.com>
2024-08-12 21:58:49 -0300

    Clase 2

--- app/src/main/java/com/example/clase1/MainActivity.kt
+++ app/src/main/java/com/example/clase1/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.clase1
 
 import android.os.Bundle
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button: Button
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        val texto = "Hola"
+
+        button = findViewById(R.id.buttonMain)
+        textView = findViewById(R.id.TextViewMain)
+        textView.text = "Esto es un texto"
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Se preciono el boton", Toast.LENGTH_SHORT)
+        }
+
     }
 }
